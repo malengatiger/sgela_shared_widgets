71289ef90664074d0c5fba951afbc2713b57cf94
Aubrey Malabie <aubrey@aftarobot.com>
2024-05-02 10:22:30 +0200

    refactored everything :) 🍎🍎

---
+++ lib/widgets/subject_filter.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:sgela_services/data/subject.dart';
+import 'package:sgela_services/sgela_util/functions.dart';
+import 'package:sgela_shared_widgets/util/gaps.dart';
+import 'package:sgela_shared_widgets/util/styles.dart';
+
+class SubjectFilterWidget extends StatefulWidget {
+  final List<Subject> subjects;
+  final Function(Subject) onSelected;
+
+  const SubjectFilterWidget(
+      {Key? key, required this.subjects, required this.onSelected})
+      : super(key: key);
+
+  @override
+  _SubjectFilterWidgetState createState() => _SubjectFilterWidgetState();
+}
+
+class _SubjectFilterWidgetState extends State<SubjectFilterWidget> {
+  late List<Subject> filteredSubjects;
+  late TextEditingController searchController;
+  static const mm = '💜💜💜💜SubjectFilterWidget💜';
+
+  @override
+  void initState() {
+    super.initState();
+    filteredSubjects = widget.subjects;
+    searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  void filterSubjects(String query) {
+    pp('$mm filtering $query ...');
+
+    setState(() {
+      if (query.isEmpty) {
+        filteredSubjects = widget.subjects;
+      } else {
+        filteredSubjects = widget.subjects
+            .where((subject) =>
+                subject.title!.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 140,
+        child: Column(
+          children: [
+            SizedBox(
+              width: 300,
+              child: TextField(
+                controller: searchController,
+                onChanged: (m) {
+                  filterSubjects(m);
+                },
+                decoration: InputDecoration(
+                  labelText: 'Search',
+                ),
+              ),
+            ),
+            gapH16,
+            SizedBox(
+              width: 400,
+              child: DropdownButton<Subject>(
+                items: filteredSubjects.map((subject) {
+                  return DropdownMenuItem<Subject>(
+                    value: subject,
+                    child: Padding(
+                        child: Text('${subject.title!}',
+                            style: myTextStyleSmall(context)),
+                        padding: EdgeInsets.only(left:16,right:16,))
+                  );
+                }).toList(),
+                onChanged: (selectedSubject) {
+                  // Handle the selected subject
+                  if (selectedSubject != null) {
+                    widget.onSelected(selectedSubject);
+                  }
+                },
+              ),
+            )
+          ],
+        ));
+  }
+}
